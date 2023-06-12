73a22ef535a09da9b133c61de95584bb0d1a2225
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-12 14:37:30 -0500

    Creation of Routes to the components like Inicio, Portafolio, Servicios, Curriculum and Contacto

--- src/router/MisRutas.js
+++ src/router/MisRutas.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Routes, Route, BrowserRouter, NavLink } from "react-router-dom";
+import { Inicio } from '../components/Inicio';
+import { Portafolio } from '../components/Portafolio';
+import { Servicios } from '../components/Servicios';
+import { Curriculum } from '../components/Curriculum';
+import { Contacto } from '../components/Contacto';
 
 export const MisRutas = () => {
   return (
-    <div>MisRutas</div>
+    <BrowserRouter>
+        {/* HEADER Y NAVEGACIÓN */}
+
+        {/* CONTENIDO CENTRAL */}
+        <Routes>
+            <Route path='/' element={<Inicio/>} />
+            <Route path='/inicio' element={<Inicio/>} />
+            <Route path='/portafolio' element={<Portafolio/>} />
+            <Route path='/servicios' element={<Servicios/>} />
+            <Route path='/curriculum' element={<Curriculum/>} />
+            <Route path='/contacto' element={<Contacto/>} />
+        </Routes>
+
+        {/* FOOTER */}
+
+
+    </BrowserRouter>
   )
 }
