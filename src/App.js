5f02b26f127c18dbbbe57ac56c4c2e48fb582d7f
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-12 14:56:10 -0500

    layout classname

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import { Curriculum } from './components/Curriculum';
 import { MisRutas } from './router/MisRutas';
 
 function App() {
   return (
-    <>
+    <div className='layout'>
       <MisRutas/>
-    </>
+    </div>
   );
 }
 
