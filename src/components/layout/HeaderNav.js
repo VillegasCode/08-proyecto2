401c0b8f727e81f74a13004ded0f6f47d60f5e96
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-12 14:58:20 -0500

    Links like Inicio, Portafolio, Servicios, Curriculum, Contacto with NavLink created

---
+++ src/components/layout/HeaderNav.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export const HeaderNav = () => {
+  return (
+    <header>
+        <h1>
+            ALEX VILLEGAS HEADER
+        </h1>
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/inicio">Inicio</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/portafolio">Portafolio</NavLink>
+                </li>
+                <li>
+                    <NavLink to="servicios">Servicios</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/curriculum">Curriculum</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contacto">Contacto</NavLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
