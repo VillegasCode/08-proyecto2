80f53c5c3771864df3c987f387d407f72a1e0a76
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-12 21:47:41 -0500

    Import trabajos from dat/trabajos

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { trabajos } from '../data/trabajos'
 
 export const Portafolio = () => {
   return (
     <div className='page'>
       <h1 className='heading'>PORTAFOLIO</h1>
+    
+      {
+        trabajos.map(trabajos => {
+          <article>
+            <h2>{trabajos.nombre}</h2>
+            {console.log(trabajos)};
+          </article>
+        })
+      }
     </div>
   )
 }
