8d584783fdf55926fb017e27a008843d25142ba8
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-12 20:39:18 -0500

    Text for curriculum page

--- src/components/Curriculum.js
+++ src/components/Curriculum.js
@@ -2,6 +2,23 @@ import React from 'react'
 
 export const Curriculum = () => {
   return (
-    <div>Curriculum</div>
+    <div className='page'>
+      <h1>Curriculum</h1>
+      
+      <h2>Experiencia</h2>
+      <ul>
+        <li>Desarrollador web - 2011 al 2023</li>
+      </ul>
+
+      <h2>Conocimientos</h2>
+
+      <h2>Educación</h2>
+
+      <h2>Proyectos Personales</h2>
+
+      <h2>Reconomientos y Premios</h2>
+
+      <h2>Otros</h2>
+    </div>
   )
 }
