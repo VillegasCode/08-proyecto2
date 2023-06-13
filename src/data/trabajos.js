c7298668852609ee723ee431ae7952ce85362429
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-12 21:48:05 -0500

    An array of trabajos mine

---
+++ src/data/trabajos.js
@@ -0,0 +1,33 @@
+export const trabajos = [
+   {
+        'id': 'empresarioencasa',
+        'nombre': 'Alex Villegas',
+        'url': 'empresarioencasa.org',
+        'tecnologias': 'HTML, CSS, JS, PHP, MYSQL',
+        'categorias': 'Desarrollo Web'
+   },
+
+   {
+    'id': 'facebook',
+    'nombre': 'Facebook',
+    'url': 'facebook.com',
+    'tecnologias': 'React, Node, Mongo',
+    'categorias': 'Desarrollo Web'
+    },
+
+    {
+        'id': 'twitter',
+        'nombre': 'Facebook',
+        'url': 'facebook.com',
+        'tecnologias': 'React, Node, Mongo',
+        'categorias': 'Desarrollo Web'
+        },
+
+    {
+            'id': 'LinkedIn',
+            'nombre': 'Facebook',
+            'url': 'facebook.com',
+            'tecnologias': 'React, Node, Mongo',
+            'categorias': 'Desarrollo Web'
+    },
+];
