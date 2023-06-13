8abbfdb0302b39e62f0ac6ac1f5f13d6a016aa55
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-12 20:28:05 -0500

    Servicios page added

--- src/components/Servicios.js
+++ src/components/Servicios.js
@@ -2,6 +2,24 @@ import React from 'react'
 
 export const Servicios = () => {
   return (
-    <div>Servicios</div>
+    <div className='page'>
+      <h1>SERVICIOS</h1>
+      <section className='services'>
+          <article className='service'>
+            <h2>Diseño Web</h2>
+            <p>Hago que tu web se vea bonita</p>
+          </article>
+
+          <article className='service'>
+            <h2>Desarrollo Web</h2>
+            <p>Creo tu aplicación web desde cero</p>
+          </article>
+
+          <article className='service'>
+            <h2>Posicionamiento Web</h2>
+            <p>Hago que tu web aparezca en Google y la vea tu público objetivo</p>
+          </article>
+      </section>
+    </div>
   )
 }
