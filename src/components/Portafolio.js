bc148d451021806c2f115fb480d149dddc6081db
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-14 20:52:13 -0500

    importing ListadoTrabajos component

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -1,28 +1,12 @@
 import React from 'react'
-import { trabajos } from '../data/trabajos'
-import { Link } from 'react-router-dom'
+import { ListadoTrabajos } from './ListadoTrabajos'
 
 export const Portafolio = () => {
   return (
     <div className='page'>
       <h1 className='heading'>PORTAFOLIO</h1>
 
-      <section className='works'>
-      {
-        trabajos.map(trabajos => {
-          return(
-            <article key={trabajos.id} className='work-item'>
-              <div className='mask'>
-                <img src={"/images/"+trabajos.id+".png"} />
-              </div>
-              <span>{trabajos.categorias}</span>
-              <h2><Link to={"/proyecto/"+trabajos.id}>{trabajos.nombre}</Link></h2>
-              <h3>{trabajos.tecnologias}</h3>
-            </article>
-          )
-        })
-      }
-      </section>
+      <ListadoTrabajos/>
 
     </div>
   )
